1673195832544e2235530f956747811c7c320db2
rraass1233 <107373090+rraass1233@users.noreply.github.com>
2022-06-13 01:52:02 +0300

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,5 @@
+basic.show_icon(IconNames.DUCK)
+
+def on_forever():
+    basic.show_string("Abeer Alonazi 1-1")
+basic.forever(on_forever)
---
+++ main.ts
@@ -0,0 +1,4 @@
+basic.showIcon(IconNames.Duck)
+basic.forever(function () {
+    basic.showString("Abeer Alonazi 1-1")
+})
